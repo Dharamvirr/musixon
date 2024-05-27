aaa7d739880d3842f77b5c7f173975065afa1ba1
dharamvir <dharamvir78914@gmail.com>
2024-05-27 18:07:54 +0530

    musixon

---
+++ index.js
@@ -0,0 +1,36 @@
+//blinding
+var blindingg = new Audio("music/blindinglights.mp3");
+var a=document.getElementById("blinding");
+a.addEventListener('click',function()
+{
+	blindingg.play();
+});
+ //killshot
+var killshotg = new Audio("music/killshot.mp3");
+var b=document.getElementById("killshot");
+b.addEventListener('click',function()
+{
+	killshotg.play();
+});
+//carnival
+var carnivalg = new Audio("music/carnival.mp3");
+var c=document.getElementById("carnival");
+c.addEventListener('click',function()
+{
+	carnivalg.play();
+});
+//kesariya
+var kesariyag = new Audio("music/kesariya.mp3");
+var d=document.getElementById("kesariya");
+d.addEventListener('click',function()
+{
+	kesariyag.play();
+});
+//loveurself
+var loveurselfg = new Audio("music/loveurself.mp3");
+var e=document.getElementById("loveurself");
+e.addEventListener('click',function()
+{
+	loveurselfg.play();
+});
+ 
